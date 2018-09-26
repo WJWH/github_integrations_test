6f949a381afffbe3c6a989b448762d2adf772446
Wander Hillen <wjw.hillen@gmail.com>
2018-09-26 20:18:29 +0200

    Initial commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'bundler'
+require 'json'
+
+Bundler.require
+
+GITHUB_SECRET = 'omg_dat_secret' # So eh yeah, replace this with something more secure in your own thing
+
+class WebhookTester < Sinatra::Base
+  get '/' do
+    'biep'
+  end
+  
+  post '/event_handler' do
+    @payload = JSON.parse(params[:payload])
+  
+    case request.env['HTTP_X_GITHUB_EVENT']
+    when "push"
+      if @payload["action"] == "opened"
+        process_pull_request(@payload["pull_request"])
+      end
+    end
+  end
+end
